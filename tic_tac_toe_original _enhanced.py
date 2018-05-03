4d102ec3ea2416cb6d61de6398f593cb0bcdd865
ujLevente <asdlevente@gmail.com>
2018-05-03 13:13:50 +0200

    tictactoe enhanted

--- tic_tac_toe_original _enhanced.py
+++ tic_tac_toe_original _enhanced.py
@@ -1,4 +1,6 @@
-board = list(range(1,10))
+board = list(range(1, 10))
+turn_counter = 1
+
 
 def draw_board():
     print(""" 
@@ -6,100 +8,48 @@ def draw_board():
         ---------  
         {} | {} | {}   
         ---------  
-        {} | {} | {}""".format(*board))
+        {} | {} | {}
+        """.format(*board))
+
 
 def win():
     for i in range(0, len(board), 3):
         if len(set(board[i:i+3])) == 1:
-            print("win")
+            return True
     for i in range(0, 3):
         if len(set(board[i::3])) == 1:
-            print("win")
+            return True
     if len(set(board[0::4])) == 1:
-        print("win")
-    elif len(set(board[2:7:2])) == 1:
-        print("win")
-
-# players turns
-
-def p1_turn():
-    p1 = input("Player 1! Enter a number for the desired slot: ")
-    p1 = int(p1)
-
-    if p1 > 9 or p1 < 0:
-        p1 = input("Enter a number between 1-9: ")
-        p1 = int(p1)
-        board[p1-1] = "X"
-        draw_board()
-    elif board[p1-1] == "O":
-        p1 = input("Player 1! Enter another slot: ")
-        p1 = int(p1)
-        board[p1-1] = "X"
-        draw_board()
-    else:   
-        board[p1-1] = "X"
-        draw_board()
-
-
-def p2_turn():
-    p2 = input("Player 2! Enter a number for the desired slot: ")
-    p2 = int(p2)
-
-    if p2 > 9 or p2 < 0:
-        p2 = input("Enter a number between 1-9: ")
-        p2 = int(p2)
-        board[p2-1] = "O"
-        draw_board()    
-    elif board[p2-1] == "X":
-        p2 = input("Player 2! Enter another slot: ")
-        p2 = int(p2)
-        board[p2-1] = "O"
-        draw_board()
-    else:
-        board[p2-1] = "O"
-        draw_board()
-
-#check if board is full
-
-def full_board():
-    fullx = board.count("X")
-    fullo = board.count("O")
-    if fullx + fullo == 9:
         return True
-    
+    elif len(set(board[2:7:2])) == 1:
+        return True
+
 
 draw_board()
 
-#START GAME
-
-while True:
-    while True:
-        try:
-            p1_turn()
-            break
-        except:
-            print("Error! Please enter a valid input!")
-        
-
-    x_win()
-    if x_win() == True:
-        print("Player 1 wins!")
-        break
-    
-    full_board()
-    if full_board() == True:
-        print("The board is full!")
-        break
+# START GAME
+while turn_counter <= 9:
+    if turn_counter % 2 == 0:
+        mark = "O"
+    else:
+        mark = "X"
 
     try:
-        p2_turn()
-    except:
+        player = int(input("Player" + mark + "! Enter a number for the desired slot: ")) - 1
+        assert player >= 0
+
+        if str(board[player]).isnumeric():
+            board[player] = mark
+            draw_board()
+        else:
+            raise ValueError
+
+    except (IndexError, ValueError, AssertionError):
         print("Error! Please enter a valid input!")
-        p2_turn()
-    
-    o_win()
-    if o_win() == True:
-        print("Player 2 wins!")
+        continue
+
+    if win():
+        print("win")
         break
 
-        
+    turn_counter += 1
